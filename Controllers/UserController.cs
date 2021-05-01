b9bb07cc6438f448de424d071d4927696885df36
Luciano Zang <zangeronimo@gmail.com>
2021-05-01 10:58:48 -0300

    initial commit

---
+++ Controllers/UserController.cs
@@ -0,0 +1,121 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using api.Data;
+using api.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace api.Controllers
+{
+
+    [ApiController]
+    public class UserController : Controller
+    {
+        [HttpGet("v1/users")]
+        public async Task<ActionResult<List<User>>> Get(
+            [FromServices] DataContext context,
+            [FromQuery(Name = "filter")] string filter)
+        {
+            try
+            {
+                var users = context.users.AsQueryable();
+
+                // Se existe filtro, aplico à consulta
+                if (!string.IsNullOrEmpty(filter))
+                {
+                    var clause = filter.ToLower();
+
+                    users = users
+                        .Where(x => x.Name.ToLower().Contains(clause)
+                                || x.Email.ToLower().Contains(clause));
+                }
+
+                return await users.ToListAsync();
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+        [HttpPost("v1/users")]
+        public async Task<ActionResult<User>> Post(
+            [FromServices] DataContext context,
+            [FromBody] User model)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    context.users.Add(model);
+                    await context.SaveChangesAsync();
+                    return model;
+                }
+                else
+                {
+                    return BadRequest(ModelState);
+                }
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+
+        [HttpPut("v1/users/{id}")]
+        public async Task<ActionResult<User>> Put(
+            [FromServices] DataContext context,
+            int id,
+            [FromBody] User model)
+        {
+            try
+            {
+                var user = await context.users.FindAsync(id);
+                if (user == null)
+                {
+                    return BadRequest();
+                }
+
+                user.Name = model.Name;
+                user.Email = model.Email;
+
+                // Adiciono updated_at ao contato
+                user.UpdatedAt = DateTime.Now;
+
+                context.users.Update(user);
+                await context.SaveChangesAsync();
+
+                return Ok(user);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+
+        [HttpDelete("v1/users/{id}")]
+        public async Task<ActionResult<User>> Delete(
+            [FromServices] DataContext context,
+            int id)
+        {
+            try
+            {
+                var user = await context.users.FindAsync(id);
+                if (user == null)
+                {
+                    return BadRequest();
+                }
+
+                context.users.Remove(user);
+                await context.SaveChangesAsync();
+
+                return Ok(user);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,27 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace api.Models
+{
+    [Table("users")]
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [MinLength(3, ErrorMessage = "Este campo deve conter entre 3 e 150 caracteres")]
+        [MaxLength(150, ErrorMessage = "Este campo deve conter entre 3 e 150 caracteres")]
+        public string Name { get; set; }
+
+        [MinLength(3, ErrorMessage = "Este campo deve conter entre 3 e 150 caracteres")]
+        [MaxLength(150, ErrorMessage = "Este campo deve conter entre 3 e 150 caracteres")]
+        public string Email { get; set; }
+
+        [Column("created_at")]
+        public DateTime? CreatedAt { get; set; }
+
+        [Column("updated_at")]
+        public DateTime? UpdatedAt { get; set; }
+    }
+}
